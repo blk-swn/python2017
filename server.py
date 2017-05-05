a0f390ef1b43d8a142b502e0379df6aeecebc737
Imran Thompson <imranthom@gmail.com>
2017-05-05 20:33:11 +1000

    Added some comments

--- server.py
+++ server.py
@@ -1,21 +1,35 @@
 from socket import *
 import pickle
 from threading import Thread
+'''
+    This is the server class it handles everything on the server side.
+'''
 
 
 class serverTcp():
 
     def __init__(self):
+        '''
+            This is the initializer '__init__' method. When i create an instance of the class
+            at the bottom of the file, this method is automatically called and it just initializes
+            some variables so we can access them throughout the class. the variables with self. in-front
+            of them are like global variables accessible from anywhere in the serverTcp class.
+        '''
         self.host = ''
         self.port = 5001
-        self.soc = None
+        self.soc = socket(AF_INET, SOCK_STREAM)
+        self.soc.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
         self.userList = []
         self.connections = []
 
     def start(self):
-        self.soc = socket(AF_INET, SOCK_STREAM)
-        self.soc.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-
+        '''
+            This method opens the socket and accepts client connections.
+            When a client connects inside the while True: block, a new thread
+            is created and the clients connection is passed on to the main client
+            function tcpLink() which will handle authorising the user and so on.
+        '''
         self.soc.bind((self.host, self.port))
         self.soc.listen(10)
         print("socket created...")
@@ -27,23 +41,23 @@ class serverTcp():
             t.start()
             self.connections.append(t)
 
-    def tcpLink(self, con: socket, addr: tuple):
+    def tcpLink(self, con: socket, address: tuple):
         auth = False
         count = 0
         user = []
 
-        print("Connection from: %s" % str(addr))
+        print("Connection from: %s" % str(address))
         print("starting chat for the client")
-        self.chat(con, addr)
+        self.chat(con, address)
 
-    def chat(self, con: socket, addr: tuple):
+    def chat(self, con: socket, address: tuple):
         while True:
             msg = self.readMsg(con)
             if msg in ['q', 'exit', 'quit']:
                 break
             print(msg)
             self.writeMsg(con, msg)
-        print("client %s disconnecting..." % str(addr))
+        print("client %s disconnecting..." % str(address))
         self.openFile()
         con.close()
 
@@ -73,6 +87,10 @@ class serverTcp():
             print("ERROR [writeMsg] %s " % str(e))
 
     def openFile(self):
+        '''
+            This function opens the file, creates a list out of the contents
+            and returns the list to the caller
+        '''
         try:
             f = open('auth.dat', 'r')
             try:
@@ -85,9 +103,9 @@ class serverTcp():
         except IOError:
             print("Cannot find/read the file.")
 
+'''
+    The end of the serverTcp() class. 
+'''
 
-
-
-
-server = serverTcp()
-server.start()
+server = serverTcp()    # Create an instance of the serverTcp() class
+server.start()          # Run the start method.
